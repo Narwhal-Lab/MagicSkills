f7a82a5515011651d51207dd7fe42d0bc31e9d2b
Y0uYuGe <3592569640@qq.com>
2026-03-12 12:11:42 +0800

    logo

--- src/magicskills/utils/agents_md.py
+++ src/magicskills/utils/agents_md.py
@@ -51,7 +51,7 @@ def generate_skills_xml(skills: Iterable[Skill], invocation: str) -> str:
         "<usage>\n"
         "When users ask you to perform tasks, check if any of the available skills below can help complete the task more effectively.\n\n"
         "How to use skills:\n"
-        'Unified skill cli. If you are not sure, you can first use "magicskills listskill" '
+        'Unified skill cli. If you are not sure, you can first use "magicskills listskill " '
         "to search for available skills. Then, determine which skill might be the most useful. "
         'After that, try to use "magicskills readskill <path>" to read the SKILL.md file under '
         "this skill path to get more detailed information. Finally, based on the content of this "
